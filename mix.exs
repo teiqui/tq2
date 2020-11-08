0163fff31767b9e1b21287471d5001299bb5b5ae
Franco Catena <francocatena@gmail.com>
2020-11-08 10:33:19 -0300

    Back to Elixir 1.11

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Tq2.MixProject do
     [
       app: :tq2,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
