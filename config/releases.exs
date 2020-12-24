278ad2fb1dad7fe2ec7decdc7e14cb3945705649
Franco Catena <francocatena@gmail.com>
2020-12-24 18:59:05 -0300

    Config using environment variables to releases.exs

--- config/releases.exs
+++ config/releases.exs
@@ -24,11 +24,38 @@ secret_key_base =
     """
 
 config :tq2, Tq2Web.Endpoint,
+  url: [
+    host: Enum.join([System.get_env("APP_SUBDOMAIN", "app"), "teiqui.com"], "."),
+    scheme: "https",
+    port: 443
+  ],
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
   secret_key_base: secret_key_base
+
+# Public store's subdomain
+config :tq2, store_subdomain: System.get_env("STORE_SUBDOMAIN", "store")
+
+# Sentry config
+config :sentry, dsn: System.get_env("SENTRY_DSN")
+
+# MercadoPago Credentials
+config :tq2, :mp,
+  ars_token: System.get_env("MP_ARS_TOKEN", ""),
+  clp_token: System.get_env("MP_CLP_TOKEN", ""),
+  cop_token: System.get_env("MP_COP_TOKEN", ""),
+  mxn_token: System.get_env("MP_MXN_TOKEN", ""),
+  pen_token: System.get_env("MP_PEN_TOKEN", "")
+
+# Exq config
+config :exq,
+  url: System.get_env("REDIS_URL", "redis://localhost:6379")
+
+# GDrive secrets
+config :goth,
+  json: System.get_env("CREDENTIALS_PATH", "config/credentials.sample.json") |> File.read!()
 
 # ## Using releases (Elixir v1.9+)
 #
