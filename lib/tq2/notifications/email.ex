0bfc960169e7a6008ae6625834937b7db41a56fb
Franco Catena <francocatena@gmail.com>
2020-12-27 14:55:50 -0300

    No more gettext for Teiqui brand on email from

--- lib/tq2/notifications/email.ex
+++ lib/tq2/notifications/email.ex
@@ -42,7 +42,7 @@ defmodule Tq2.Notifications.Email do
     address = System.get_env("EMAIL_ADDRESS", "support@teiqui.com")
 
     new_email()
-    |> from({gettext("Teiqui"), address})
+    |> from({"Teiqui", address})
     |> put_layout({Tq2Web.LayoutView, :email})
   end
 end
