6bfd51a5f54fa23d3575747d2fc21e97a3dde89c
Franco Catena <francocatena@gmail.com>
2021-01-02 21:34:28 -0300

    Little improvement on store form

--- lib/tq2_web/views/store_view.ex
+++ lib/tq2_web/views/store_view.ex
@@ -2,6 +2,19 @@ defmodule Tq2Web.StoreView do
   use Tq2Web, :view
   use Scrivener.HTML
 
+  defp public_store_link(store) do
+    url = store_uri() |> Routes.counter_url(:index, store)
+
+    link(url, to: url) |> safe_to_string()
+  end
+
+  defp store_slug_hint do
+    dgettext(
+      "stores",
+      "This is part of the store address, you can only use letters, underscores and numbers, don't use spaces!"
+    )
+  end
+
   def lock_version_input(_, nil), do: nil
 
   def lock_version_input(form, store) do
@@ -16,4 +29,14 @@ defmodule Tq2Web.StoreView do
 
   defp submit_label(nil), do: dgettext("stores", "Create")
   defp submit_label(_), do: dgettext("stores", "Update")
+
+  defp store_uri do
+    scheme = if Tq2Web.Endpoint.config(:https), do: "https", else: "http"
+    url_config = Tq2Web.Endpoint.config(:url)
+
+    %URI{
+      scheme: scheme,
+      host: Enum.join([Application.get_env(:tq2, :store_subdomain), url_config[:host]], ".")
+    }
+  end
 end
