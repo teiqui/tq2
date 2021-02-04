22f5083f16a3ba695c6bfc580a2d047c338efcaf
Franco Catena <francocatena@gmail.com>
2021-02-04 12:00:03 -0300

    Facebook pixel tracking

--- lib/tq2_web/views/layout_view.ex
+++ lib/tq2_web/views/layout_view.ex
@@ -105,4 +105,42 @@ defmodule Tq2Web.LayoutView do
       """
     end
   end
+
+  defp facebook_pixel_script do
+    if Application.get_env(:tq2, :env) == :prod do
+      ~E"""
+        <script>
+          !function (f, b, e, v, n, t, s) {
+            if (f.fbq) return
+
+            n = f.fbq = function () {
+              n.callMethod ? n.callMethod.apply(n,arguments) : n.queue.push(arguments)
+            }
+
+            if (! f._fbq) f._fbq = n
+
+            n.push    = n
+            n.loaded  = !0
+            n.version = '2.0'
+            n.queue   = []
+            t         = b.createElement(e)
+            t.async   = !0
+            t.src     = v
+            s         = b.getElementsByTagName(e)[0]
+
+            s.parentNode.insertBefore(t, s)
+          }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js')
+
+          fbq('init', '229888358772182')
+          fbq('track', 'PageView')
+        </script>
+        <noscript>
+          <img height="1"
+               width="1"
+               style="display:none"
+               src="https://www.facebook.com/tr?id=229888358772182&ev=PageView&noscript=1">
+        </noscript>
+      """
+    end
+  end
 end
