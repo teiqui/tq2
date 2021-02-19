1b6ff2af2c16c11357197e0b72d41608e5fb6fd0
Franco Catena <francocatena@gmail.com>
2021-02-18 21:33:40 -0300

    Complete payment gettext messages

--- lib/tq2_web/views/order_view.ex
+++ lib/tq2_web/views/order_view.ex
@@ -26,7 +26,7 @@ defmodule Tq2Web.OrderView do
     "cash" => dgettext("payments", "Cash"),
     "mercado_pago" => dgettext("payments", "MercadoPago"),
     "other" => dgettext("payments", "Other"),
-    "transbank" => dgettext("payments", "Transbank - Onepay"),
+    "transbank" => dgettext("payments", "Transbank - OnePay"),
     "wire_transfer" => dgettext("payments", "Wire transfer")
   }
 
