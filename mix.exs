17e85a1592eb68a0d2ca41f2093cb722f0fca1a7
Franco Catena <francocatena@gmail.com>
2021-03-14 10:40:08 -0300

    Remove reference comments from Gettext

--- mix.exs
+++ mix.exs
@@ -11,7 +11,8 @@ defmodule Tq2.MixProject do
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
-      releases: releases()
+      releases: releases(),
+      gettext: [write_reference_comments: false]
     ]
   end
 
